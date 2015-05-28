67a5eea87e39ac4d56ed469fe794337a2122b842
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-05-28 16:51:48 -0600

    added main_dish method

--- lunch.rb
+++ lunch.rb
@@ -2,6 +2,18 @@ class Lunch
 
 	def initialize
 		puts "What would you like for lunch today?"
+		main_dish
 	end
 
-end
+	def main_dish
+		main = {"steak" => 10.00, "pasta" => 7.00, "pizza" => 5.00}
+		puts "Your choices for main dish are..."
+		main.each_key {|key| puts key}
+		puts "Which would you like?"
+		user_input = gets.chomp
+		side_dishes
+	end
+
+
+lunch = Lunch.new
+lunch
